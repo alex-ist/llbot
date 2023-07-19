5df0bd5bdf286f33867ad0086244dc43b8feecff
Aleksei.Istomin <Aleksei.Istomin@acronis.com>
2023-07-19 17:38:22 +0200

    Support running the administration interface on Windows

---
+++ adm/admin.py
@@ -0,0 +1,60 @@
+
+from datetime import datetime
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+from adm.adm_utils import get_last_n_lines
+from utils import *
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.getcwd(), "data", "ll.db")
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    user_id = db.Column('user_id', db.Integer, primary_key=True)
+    username = db.Column('username', db.String)
+    first_name = db.Column('first_name', db.String)
+    name = db.Column('name', db.String)
+    lang_code = db.Column('lang_code', db.String)
+    first_access = db.Column('first_access', db.Integer)
+    last_access = db.Column('last_access', db.Integer)
+    shown_words_count = db.Column('shown_words_count', db.Integer)
+    current_forget_rate = db.Column('current_forget_rate', db.Float)
+    def get_forget_rate(self):
+        if self.current_forget_rate is None:
+            return -1
+        else:
+            return format(self.current_forget_rate*100, ".1f")
+
+    def get_first_access(self):
+        return datetime.fromtimestamp(self.first_access).strftime('%Y-%m-%d %H:%M')
+    def get_last_access(self):
+        return datetime.fromtimestamp(self.last_access).strftime('%Y-%m-%d %H:%M')
+    
+class Word(db.Model):
+    __tablename__ = 'words'
+    word_id = db.Column('word_id', db.Integer, primary_key=True)
+    user_id = db.Column('user_id', db.Integer, db.ForeignKey('users.user_id'))
+    foreign_w = db.Column('foreign_w', db.String)
+    native_w = db.Column('native_w', db.String)
+    example = db.Column('example', db.String)
+
+@app.route('/')
+def show_users():
+    users = User.query.all()
+    return render_template('users.html', users=users)
+
+@app.route('/words/<int:user_id>')
+def show_user_words(user_id):
+    words = Word.query.filter_by(user_id=user_id).all()
+    return render_template('words.html', user_id=user_id, words=words)
+
+@app.route('/log/<int:user_id>')
+def show_user_log(user_id):
+    log=get_last_n_lines('log/ll.log', user_id, 100)
+    return render_template('log.html', user_id=user_id, lines=log)
+
+if __name__ == '__main__':
+    app.run(debug=True)
