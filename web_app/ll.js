39672d1fd0c7304d633972446f70a2e0b2599b32
Aleksei.Istomin <Aleksei.Istomin@acronis.com>
2023-09-21 00:15:47 +0200

    Move Mini App communication to WebSockets

--- web_app/ll.js
+++ web_app/ll.js
@@ -1,25 +1,26 @@
 let tg=window.Telegram.WebApp;
 tg.ready()
-let f=document.getElementById("flash");
+const socket = new WebSocket('wss://lingolink.bot.nu/tren-wh/');
+let chat_id = "484679683";    
 
+
+function startConn() {
+    const dataToSend = {chat_id: chat_id};
+    socket.send(JSON.stringify(dataToSend));
+}
+
+function sendMessage(msg) {
+    const dataToSend = {value: msg};
+    socket.send(JSON.stringify(dataToSend));
+}
+
+socket.addEventListener('open', () => {
+    startConn()
+});
+
+let f=document.getElementById("flash");
 f.addEventListener("click",()=> {
-    let chat_id = tg?.initDataUnsafe?.user?.id || "484679683";    
-    const dataToSend = {
-        value: 'click',
-        //query_id: tg.initDataUnsafe.query_id, 
-        chat_id: chat_id
-    };
-    // Отправить данные на сервер
-    //fetch("https://ll.du:8000/tren", { 
-    fetch("https://lingolink.bot.nu/tren-wh/", { 
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(dataToSend)
-    })
-    .then(response => console.log(response)) // Вывести результат в консоль
-    .catch(error => console.error('Error:', error)); // Обработка ошибок
+    sendMessage("click")
     tg.close()
 });
 
--- webapp_hook.py
+++ webapp_hook.py
@@ -2,7 +2,7 @@
 import asyncio
 import ssl
 from botlog import logger
-from aiohttp import web
+from aiohttp import web, WSMsgType
 import json
 
 
@@ -14,34 +14,48 @@ async def handle_options(request):
     })
 
 from lingo import send_w, send_b
-async def handle_post(request):
-    try:
-        logger.warning(f"Received req: {request}")
-        payload = await request.json()
-        value = payload.get('value')
-        #query_id = payload.get('query_id')
-        chat_id = payload.get('chat_id')
-        logger.warning(f"Received data: {payload}")
-        #logger.warning(f"send_w {query_id}, {value}")
-        #await send_w(query_id, value)
-        await send_b(chat_id, value)
-        # Здесь можно добавить логику для работы с данными и вызова вашего бота
 
-        return web.Response(text="OK",  headers={
-            'Access-Control-Allow-Origin': '*',
-            'Content-Type': 'text/plain'
-        })
-    
+async def websocket_handler(request):
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+    chat_id = None
+    logger.warning("websocket_handler")
 
-    except json.JSONDecodeError:
-        logger.warning(f"Resp status=400 Invalid JSON")
+    async for msg in ws:
+        try:
+            if msg.type == WSMsgType.TEXT:
+                logger.warning(f"{msg.data}")
 
-        return web.Response(status=400, text="Invalid JSON")
+                parsed_data = json.loads(msg.data)
+                if chat_id is None:
+                    chat_id = parsed_data.get('chat_id')
+                    await send_b(chat_id, 'app open')
+                else:
+                    v = parsed_data.get('value')
+                    await send_b(chat_id, v)
+            elif msg.type == WSMsgType.ERROR:
+                err_msg=f'ws err = {ws.exception()}'
+                if chat_id:
+                    await send_b(chat_id, err_msg)
+                logger.warning(err_msg)
+        except json.JSONDecodeError:
+            err_msg=f'ws err = Invalid JSON'
+            if chat_id:
+                await send_b(chat_id, err_msg)
+            logger.warning(err_msg)
+        except Exception as e:
+            logger.warning(f"An unexpected error occurred: {e}")
+
+    if ws.closed:
+        if chat_id:
+            await send_b(chat_id, "app closed")
+        logger.warning(f"WebSocket connection closed. Close code: {ws.close_code}")
+    return ws
 
 def init_app():
     app = web.Application()
     app.router.add_route('OPTIONS', '/tren-wh/', handle_options)
-    app.router.add_post('/tren-wh/', handle_post)
+    app.router.add_get('/tren-wh/', websocket_handler)
     return app
 
 async def webapp_hook_run(production_bot):
@@ -49,8 +63,5 @@ async def webapp_hook_run(production_bot):
     app=init_app()
     runner = web.AppRunner(app)
     await runner.setup()
-
-    #ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-    #ssl_context.load_cert_chain('keys/server.crt', 'cert/server.key')
     site = web.TCPSite(runner, port=8001) #, ssl_context=ssl_context)
     await site.start()
